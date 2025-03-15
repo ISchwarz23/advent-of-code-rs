92d54182b981d707c510851d2137f5ff2a9a5f78
Ingo Schwarz <ingo.schwarz23@gmail.com>
2025-03-15 14:40:23 +0100

    implement solution day 3 - part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -2,17 +2,29 @@ use regex::Regex;
 
 advent_of_code::solution!(3);
 
+const MUL_PATTERN: &str = r"mul\((\d{1,3}),(\d{1,3})\)";
+
 pub fn part_one(input: &str) -> Option<u64> {
-    let mul_pattern = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    let result: i32 = mul_pattern.captures_iter(input)
-        .map(|mul| (mul[1].parse::<i32>().unwrap(), mul[2].parse::<i32>().unwrap()))
+    let result: i32 = Regex::new(MUL_PATTERN).unwrap()
+        .captures_iter(input)
+        .map(|mul| {
+            (
+                mul[1].parse::<i32>().unwrap(),
+                mul[2].parse::<i32>().unwrap(),
+            )
+        })
         .map(|(f1, f2)| f1 * f2)
         .sum();
     Some(result as u64)
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let result = input
+        .split("do()")
+        .map(|s| s.split_once("don't()").map_or(s, |(before, _)| before))
+        .map(|s| part_one(s).unwrap())
+        .sum();
+    Some(result)
 }
 
 #[cfg(test)]
@@ -28,6 +40,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(48));
     }
 }
