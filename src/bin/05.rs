bcf87447418ab3eca0050b4b3d562b93a4a45f63
Ingo Schwarz <ingo.schwarz23@gmail.com>
2025-03-16 13:08:24 +0100

    implement solution day 5 - part 1

---
+++ src/bin/05.rs
@@ -0,0 +1,74 @@
+use std::cmp::Ordering;
+use std::collections::HashMap;
+
+advent_of_code::solution!(5);
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let page_ordering_rules: HashMap<(u64, u64), Ordering> = input
+        .lines()
+        .take_while(|line| !line.is_empty())
+        .map(|line| line.split_once("|").unwrap())
+        .flat_map(|(first, second)| {
+            let first = first.parse::<u64>().unwrap();
+            let then = second.parse::<u64>().unwrap();
+            vec![
+                ((first, then), Ordering::Less),
+                ((then, first), Ordering::Greater),
+            ]
+        })
+        .collect();
+
+    let updates: Vec<Vec<u64>> = input
+        .lines()
+        .skip_while(|line| !line.is_empty())
+        .skip(1)
+        .map(|line| {
+            line.split(',')
+                .map(|page| page.parse::<u64>().unwrap())
+                .collect()
+        })
+        .collect();
+
+    let result: u64 = updates
+        .into_iter()
+        .filter(|pages| check_in_order(pages, &page_ordering_rules))
+        .map(|pages| pages[pages.len() / 2])
+        .sum();
+    Some(result)
+}
+
+pub fn part_two(_input: &str) -> Option<u64> {
+    None
+}
+
+fn check_in_order(pages: &Vec<u64>, ordering_rules: &HashMap<(u64, u64), Ordering>) -> bool {
+    let mut copy = pages.clone();
+    order(&mut copy, ordering_rules);
+    &copy == pages
+}
+
+fn order(pages: &mut Vec<u64>, ordering_rules: &HashMap<(u64, u64), Ordering>) {
+    pages.sort_by(|first, second| {
+        ordering_rules
+            .get(&(*first, *second))
+            .map(|ord| *ord)
+            .unwrap_or(Ordering::Equal)
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(143));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
