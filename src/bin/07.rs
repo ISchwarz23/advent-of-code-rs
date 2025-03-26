bdca514a2a1bde9e5671242203777c44c4c35c02
Ingo Schwarz <ingo.schwarz23@gmail.com>
2025-03-26 21:21:48 +0100

    implement solution day 7 - part 2

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -2,30 +2,39 @@ use std::collections::HashSet;
 
 advent_of_code::solution!(7);
 
-pub fn part_one(input: &str) -> Option<u64> {
-    let result = input.lines()
-        .map(|line| parse(line))
-        .filter(|(test_value, numbers)| can_be_solved(test_value, numbers, &[Operators::add, Operators::multiply]))
-        .map(|(test_value, _)| test_value)
-        .sum();
-    Some(result)
-}
 
-struct Operators;
-impl Operators {
+struct Operations;
+impl Operations {
     fn add(first: &u64, second: &u64) -> u64 {
         first + second
     }
     fn multiply(first: &u64, second: &u64) -> u64 {
         first * second
     }
+    fn concat(first: &u64, second: &u64) -> u64 {
+        format!("{}{}", first, second).parse::<u64>().unwrap()
+    }
 }
 
 
-
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_one(input: &str) -> Option<u64> {
+    let operations = [Operations::add, Operations::multiply];
+    Some(solve(input, &operations))
 }
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let operations = [Operations::add, Operations::multiply, Operations::concat];
+    Some(solve(input, &operations))
+}
+
+fn solve(input: &str, operations: &[fn(&u64, &u64) -> u64]) -> u64 {
+    input.lines()
+        .map(|line| parse(line))
+        .filter(|(test_value, numbers)| can_be_solved(test_value, numbers, operations))
+        .map(|(test_value, _)| test_value)
+        .sum()
+}
+
 
 fn parse(line: &str) -> (u64, Vec<u64>) {
     let parts: Vec<&str> = line.split(": ").collect();
@@ -34,14 +43,15 @@ fn parse(line: &str) -> (u64, Vec<u64>) {
     (test_value, remaining_numbers)
 }
 
-fn can_be_solved(test_value: &u64, numbers: &Vec<u64>, math_operations: &[fn(&u64, &u64) -> u64]) -> bool {
+fn can_be_solved(test_value: &u64, numbers: &Vec<u64>, operations: &[fn(&u64, &u64) -> u64]) -> bool {
     let mut results = HashSet::from([numbers.first().unwrap().clone()]);
 
     for next_number in numbers.iter().skip(1) {
         results = results.iter()
             .flat_map(|intermediate_result| {
-                math_operations.iter().map(|operation| operation(intermediate_result, next_number)).collect::<Vec<u64>>()
+                operations.iter().map(|operation| operation(intermediate_result, next_number)).collect::<Vec<u64>>()
             })
+            .filter(|result| result <= test_value)
             .collect::<HashSet<u64>>();
     }
 
@@ -62,6 +72,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11387));
     }
 }
