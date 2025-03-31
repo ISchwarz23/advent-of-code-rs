4da26db41e88b79f9d47d37011987dff28f1567b
Ingo Schwarz <ingo.schwarz23@gmail.com>
2025-03-31 12:46:50 +0200

    improve var naming in day 2 solution

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -3,8 +3,8 @@ advent_of_code::solution!(2);
 pub fn part_one(input: &str) -> Option<u64> {
     let result = input
         .lines()
-        .map(|line| parse_line_to_sequence(line))
-        .filter(|sequence| is_safe(sequence))
+        .map(|report_str| parse_report(report_str))
+        .filter(|report| is_safe(report))
         .count();
     Some(result as u64)
 }
@@ -12,39 +12,40 @@ pub fn part_one(input: &str) -> Option<u64> {
 pub fn part_two(input: &str) -> Option<u64> {
     let result = input
         .lines()
-        .map(|line| parse_line_to_sequence(line))
-        .filter(|sequence| is_safe_when_skipping(sequence))
+        .map(|report_str| parse_report(report_str))
+        .filter(|report| is_safe_when_skipping(report))
         .count();
     Some(result as u64)
 }
 
-fn parse_line_to_sequence(line: &str) -> Vec<i32> {
-    line.split(" ")
-        .map(|nr_str| nr_str.parse::<i32>().unwrap())
+fn parse_report(report_str: &str) -> Vec<i32> {
+    report_str.split(" ")
+        .map(|level_str| level_str.parse::<i32>().unwrap())
         .collect::<Vec<i32>>()
 }
 
-fn is_safe(sequence: &Vec<i32>) -> bool {
-    let safe_deltas = if is_ascending(sequence) { 1..=3 } else { -3..=-1 };
-    sequence
+fn is_safe(report: &Vec<i32>) -> bool {
+    let safe_deltas = if is_ascending(report) { 1..=3 } else { -3..=-1 };
+    report
         .windows(2)
-        .map(|pair| pair[1] - pair[0])
-        .all(|delta| safe_deltas.contains(&delta))
+        .map(|level_pair| level_pair[1] - level_pair[0])
+        .all(|level_delta| safe_deltas.contains(&level_delta))
 }
 
-fn is_safe_when_skipping(sequence: &Vec<i32>) -> bool {
-    for i in 0..sequence.len() {
-        let mut sequence_with_skipped_value = sequence.clone();
-        sequence_with_skipped_value.remove(i);
-        if is_safe(&sequence_with_skipped_value) {
+fn is_safe_when_skipping(report: &Vec<i32>) -> bool {
+    for i in 0..report.len() {
+        // could this be improved? (avoid cloning)
+        let mut report_with_skipped_level = report.clone();
+        report_with_skipped_level.remove(i);
+        if is_safe(&report_with_skipped_level) {
             return true;
         }
     }
     false
 }
 
-fn is_ascending(sequence: &Vec<i32>) -> bool {
-    sequence.first().unwrap() < sequence.last().unwrap()
+fn is_ascending(report: &Vec<i32>) -> bool {
+    report.first().unwrap() < report.last().unwrap()
 }
 
 #[cfg(test)]
